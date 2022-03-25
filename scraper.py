a051bed0b6a595c834584457e48cd888848a33f1
Gareth Jones <garetrhsion@googlemail.com>
2022-03-25 10:48:05 +0000

    Adding scraper and templates

---
+++ scraper.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+
+import requests
+from bs4 import BeautifulSoup
+
+import random
+import time
+
+import json
+
+import gspread
+from datetime import datetime
+
+
+class ClogauScraper:
+
+    def write_to_googlesheets(self, df, filename):
+        ''' Creates a google sheet, and pushes the product data from a pandas dataframe 
+            Parameters:
+                df : pandas dataframe with product data
+
+            Returns:
+                None
+        '''
+
+        try:
+            sa = gspread.service_account('service_account.json')
+            sh = sa.create(filename)
+            wks = sh.worksheet('Sheet1')
+
+            wks.update([df.columns.values.tolist()] + df.values.tolist())
+            sh.share('garethsion@googlemail.com', perm_type='user', role='writer')
+        except Exception as e:
+            print(e.message, e.args)
+
+        return 
+    
+    def run(self):
+        ''' Scrapes ClogauOutlet product pages, starting from a list of all products from the sitemap''' 
+
+        # Acquire list of all product links from website sitemap.xml
+        sitemap_url = 'https://www.clogauoutlet.co.uk/sitemap-product-0.xml.gz'
+        soup = BeautifulSoup(requests.get(sitemap_url).text, 'lxml')
+
+        url_list = []
+        for loc in soup.select('url > loc'):
+            url_list.append(loc.text)
+
+        products = []
+        skus = []
+        mpns = []
+        rrps = []
+        cur_prices = []
+        urls = []
+            
+        for url in url_list:
+
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, 'html.parser')
+            
+            prod = soup.find(name='h1', attrs={'class':"productName_title"})
+            products.append(prod.text if prod else 'None')
+
+            sku = url.split('.html')[0].split('/')[-1]
+            skus.append(sku if sku else 'None')
+
+            mpn_soup = soup.find(name='script', attrs={'type':"application/ld+json"})
+            json_object = json.loads(mpn_soup.contents[0])
+            mpns.append(json_object['mpn'] if json_object['mpn'] else 'None')
+
+            rrp = soup.find(name='p', attrs={'class':"productPrice_rrp"})
+            rrps.append(rrp.text.strip('RRP: ') if rrp else 'None')
+
+            cur_pr = soup.find(name='p', attrs={'class':"productPrice_price"})
+            cur_prices.append(cur_pr.text.strip() if cur_pr else 'None')
+
+            urls.append(url)
+            time.sleep(1 + random.random())
+
+        # Create pandas dataframe
+        data = {'Product':products,
+                    'SKU':skus,
+                    'MPN':mpns,
+                    'RRP':rrps,
+                    'CurrentPrice':cur_prices, 
+                    'URL':urls}
+        self.df = pd.DataFrame(data=data)
+
+        self.results = []
+        for i in range(len(self.df)):
+            self.results.append(self.df.iloc[i].tolist())
+        
+        now = datetime.now()
+        dt_string = now.strftime("%Y%m%d_%H%M%S")
+        self.filename = dt_string + '_CloguaGold_Product_Scrape'
+        self.write_to_googlesheets(self.df, self.filename)
+
+if __name__ == '__main__':
+    scraper = ClogauScraper()
+    scraper.run()
+
+
+
+
+
+
